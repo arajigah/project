a2ef25e8193ee8fc12c39c1a3daa6242bf288274
Cody Miller <cmiller@catalystitservices.com>
2016-01-05 09:36:31 -0800

    added some insert data into the intial sql script

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -1,1 +1,4 @@
 INSERT INTO animal (animal_name, scientific_name, informational_link)VALUES ('Sumatran orangutan','Pongo abelii', 'link')
+INSERT INTO animal (animal_name, scientific_name, informational_link)VALUES ('Monkey','Pongo abelii', 'link')
+INSERT INTO animal (animal_name, scientific_name, informational_link)VALUES ('Ape','Pongo abelii', 'link')
+INSERT INTO animal (animal_name, scientific_name, informational_link)VALUES ('Rattle Snake','Pongo abelii', 'link')
